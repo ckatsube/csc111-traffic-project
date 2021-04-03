c4c2759f0cab3be509bea918023438543b40ca81
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2021-04-03 17:22:20 +0530

    Delete datatransformation1.py

--- datatransformation1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv('trafficdata.csv')
-#tf = df['speed'] >= 0
-fd = pd.DataFrame(df, columns=['time', 'segment_id', 'speed', 'street', 'direction',
-                                'from_street', 'to_street', 'length', 'bus_count',
-                                'hour', 'day_of_week', 'month'])
-fd_one = fd[fd['speed'] != -1]
-# print(fd_one.shape) outputs (1852701, 12), all negative speed values removed
