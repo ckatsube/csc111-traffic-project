d792e2524a5085baacb048f02cdc532699cc930b
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2021-04-16 00:12:50 +0530

    Add files via upload

--- visualisation.py
+++ visualisation.py
@@ -10,19 +10,23 @@ import math
 
 # currently I have defined this list, but this list should be defined using the function call from get
 # path functions and our computational function. Someone please can make the function call here correctly
+#lst = [(41.75, -87.6), (41.85, -87.65), (41.9, -87.70), (42.00, -87.8)]
 
-lst = [(41.75, -87.6), (41.85, -87.65), (41.9, -87.70), (42.00, -87.8)]
+def visualise(lst: list):
+    """
+    Give a visual representation of the path on the map of Chicago City.
+    """
+    ruh_m = plt.imread('map1.png')
+    BBox = (41.65900629, 42.0128288601, -87.535052, -87.8367650119)  # endpoints obtained from
+                                                                     # computation in data transformation
+    fig, ax = plt.subplots(figsize=(8, 7))
 
-ruh_m =plt.imread('map1.png')
-BBox = (41.65900629, 42.0128288601, -87.535052, -87.8367650119)
-fig, ax = plt.subplots(figsize=(8, 7))
+    # use a for loop and plot the longitudes and latitudes of the path.
 
-#use a for loop and plot the longitudes and latitudes of the path. (Will need to call path functions)\
+    for point in lst:
+         ax.scatter(float(point[0]), float(point[1]), zorder=10, alpha=0.8, c='black', s=30)
 
-for point in lst:
-    ax.scatter(float(point[0]), float(point[1]), zorder=10, alpha=0.8, c='black', s=30)
-
-ax.set_title('Plotting a path on Chicago map')
-ax.set_xlim(41.65900629, 42.0128288601)
-ax.set_ylim(-87.535052, -87.8367650119)
-ax.imshow(ruh_m, zorder=0, extent=BBox, aspect='equal')
+    ax.set_title('Plotting a path on Chicago map')
+    ax.set_xlim(41.65900629, 42.0128288601)
+    ax.set_ylim(-87.535052, -87.8367650119)
+    ax.imshow(ruh_m, zorder=0, extent=BBox, aspect='equal')
