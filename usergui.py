150df6d995c17a8d775e8d18f81ec5c2e1e8edbd
KnightLatreia <omegasounddragon@gmail.com>
2021-04-17 11:50:31 -0400

    refactored usergui

--- usergui.py
+++ usergui.py
@@ -4,50 +4,18 @@
 from tkinter import Button, Frame, Label, Tk
 from typing import Callable
 
-from mediatorbuilder import MenuMediatorBuilder, NullMediatorBuilder, MediatorBuilder
-
-
-def get_input_handler_frame() -> Tk:
-    """Return the Tkinter frame that contains the widgets for inputting information"""
-
-    option_names, options = _get_options()
-
-    mmb = MenuMediatorBuilder(option_names, options)
-    for name in option_names:
-        mmb.create_option_menu(name, (name,))
-    mmb.create_option_menu("random1", ("high",))
-    mmb.create_option_list("random2", ("high",))
-
-    ifb = InputFrameBuilder()
-    ifb.set_mediator_builder(mmb)
-    ifb.set_geometry("300x200")
-    return ifb.get_frame()
-
-
-def run_input_frame() -> None:
-    """Run the default the input frame using the default settings from get_input_handler_frame
-    """
-
-    get_input_handler_frame().mainloop()
-
-
-def _get_options() -> tuple[tuple, list[tuple]]:
-    option_names = (
-        "letter",
-        "low",
-        "high"
-    )
-    options = [
-        ("a", 2, 1000),
-        ("b", 1, 2000),
-        ("d", 10, 300)
-    ]
-    return option_names, options
+from mediatorbuilder import NullMediatorBuilder, MediatorBuilder
 
 
 class InputFrameBuilder:
     """Builder class for inputting the settings for the input frame
     """
+    # Private Attributes:
+    #   - _data: the data to display to the user to select from
+    #   - _geometry: the geometry of the built frame
+    #   - _title: the title of the built frame
+    #   - _mb: the MediatorBuilder to use to let input fields interact with each other
+    #   - _command: the output function to be called when hitting Export Selection
 
     _data: list[tuple]
 
@@ -114,31 +82,3 @@ class InputFrameBuilder:
             grid(row=len(named_widgets) + 1, column=1, sticky="sew")
 
         return world
-
-
-def _filter_by_selection(selection: tuple, matrix: list[tuple]) -> list[set]:
-    """Return the column sets filtered by the selection tuple"""
-
-    columns = len(selection)
-    filtered_options = [row for row in matrix if
-                        all(row[i] == selection[i] or selection[i] == ""
-                            for i in range(columns))]
-    option_sets = _create_column_sets(filtered_options)
-
-    return option_sets
-
-
-def _create_column_sets(matrix: list[tuple]) -> list[set]:
-    """Return a list of sets such that each set is the set of all items in a column of the matrix
-    """
-
-    if len(matrix) == 0:
-        return []
-    else:
-        columns = len(matrix[0])
-        sets = [set() for _ in range(columns)]
-        for row in matrix:
-            for col, item in enumerate(row):
-                sets[col].add(item)
-
-        return sets
