312f7f8f48ebff1b3517db6dbb1e64c39fc8b4e9
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2021-04-17 17:43:11 +0530

    Add files via upload
    Added pyta and docstring

--- datatransformation.py
+++ datatransformation.py
@@ -1,59 +1,75 @@
-"""
-Program that transfers the original dataset according to our project needs.
+"""CSC111 Project: datatransformation.py
+Module Description
+==================
+Program that transforms the original dataset according to our project needs.
     - selects specific columns.
     - creates tidy data by removing undesired and unavailable speed values
     - creates a new csv file with the desired data
+Contains another computation that finds out the minimum and maximum latitude and longitude values.
+Copyright and Usage Information
+===============================
+This file is Copyright (c) 2021 Kaartik Issar, Aryaman Modi,
+Craig Katsube and Garv Sood.
 """
-
-import pandas as pd
 import csv
 import math
-df = pd.read_csv('trafficdata.csv')
-fd = pd.DataFrame(df, columns=['time', 'segment_id', 'speed', 'street', 'direction',
+import pandas as pd
+
+DF = pd.read_csv('trafficdata.csv')
+FD = pd.DataFrame(DF, columns=['time', 'segment_id', 'speed', 'street', 'direction',
                                'from_street', 'to_street', 'length', 'bus_count',
                                'hour', 'day_of_week', 'month', 'start_latitude', 'start_longitude',
                                'end_latitude', 'end_longitude'])
-fd_one = fd[fd['speed'] != -1]
-fd_two = fd_one[fd_one['speed'] != 0]
+FD_ONE = FD[FD['speed'] != -1]
+FD_TWO = FD_ONE[FD_ONE['speed'] != 0]
 # saving the dataframe as csv
-fd_two.to_csv('transformed_final.csv')
+FD_TWO.to_csv('transformed_final.csv')
 
 ####################################################################################################
 
-#finding out the dimensions of the map of chicago we are going to use.in our visualisation
+# finding out the dimensions of the map of chicago we are going to use.in our visualisation
 
-max_long = -(math.inf)
-min_long = math.inf
-max_lat = -(math.inf)
-min_lat = math.inf
-with open('transformed_final.csv') as csv_file2:
-    traffic_data = csv.reader(csv_file2)
-    next(traffic_data)  # to skip the first row
-    for row in traffic_data:
-        if float(row[15]) > max_long:
-            max_long = float(row[15])
-        if float(row[15]) < min_long:
-            min_long = float(row[15])
-        if float(row[14]) > max_lat:
-            max_lat = float(row[14])
-        if float(row[14]) < min_lat:
-            min_lat = float(row[14])
+MAXLONG = -math.inf
+MINLONG = math.inf
+MAXLAT = -math.inf
+MINLAT = math.inf
+with open('transformed_final.csv') as CSV_FILE2:
+    TRAFFIC_DATA = csv.reader(CSV_FILE2)
+    next(TRAFFIC_DATA)  # to skip the first row
+    for ROW in TRAFFIC_DATA:
+        if float(ROW[15]) > MAXLONG:
+            MAXLONG = float(ROW[15])
+        if float(ROW[15]) < MINLONG:
+            MINLONG = float(ROW[15])
+        if float(ROW[14]) > MAXLAT:
+            MAXLAT = float(ROW[14])
+        if float(ROW[14]) < MINLAT:
+            MINLAT = float(ROW[14])
 
-with open('transformed_final.csv') as csv_file2:
-    traffic_data = csv.reader(csv_file2)
-    next(traffic_data)  # to skip the first row
-    for row in traffic_data:
-        if float(row[15]) > max_long:
-            max_long = float(row[17])
-        if float(row[15]) < min_long:
-            min_long = float(row[17])
-        if float(row[14]) > max_lat:
-            max_lat = float(row[16])
-        if float(row[14]) < min_lat:
-            min_lat = float(row[16])
-end_points = [max_long, min_long, max_lat, min_lat]
+with open('transformed_final.csv') as CSV_FILE2:
+    TRAFFIC_DATA = csv.reader(CSV_FILE2)
+    next(TRAFFIC_DATA)  # to skip the first row
+    for ROW in TRAFFIC_DATA:
+        if float(ROW[15]) > MAXLONG:
+            MAXLONG = float(ROW[17])
+        if float(ROW[15]) < MINLONG:
+            MINLONG = float(ROW[17])
+        if float(ROW[14]) > MAXLAT:
+            MAXLAT = float(ROW[16])
+        if float(ROW[14]) < MINLAT:
+            MINLAT = float(ROW[16])
+END_POINTS = [MAXLONG, MINLONG, MAXLAT, MINLAT]
 
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 100,
+        'disable': ['E1136', 'E9971'],
+        'extra-imports': ['heapq', 'graph', 'abc', 'pandas', 'math', 'csv'],
+        'allowed-io': [],
+        'max-nested-blocks': 5
 
-
-
+    })
